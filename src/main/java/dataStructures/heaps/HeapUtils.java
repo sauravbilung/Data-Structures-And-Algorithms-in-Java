fb658f437a196049f42f819d8214dc5b38c8c81b
sauravbilung <saurav.bilung@gmail.com>
2024-11-07 22:35:56 +0530

    HeapSort added. Changed access modifier from private to public for swap()

--- src/main/java/dataStructures/heaps/HeapUtils.java
+++ src/main/java/dataStructures/heaps/HeapUtils.java
@@ -100,7 +100,7 @@ public class HeapUtils {
         }
     }
 
-    private static void swap(int[] arr, int i, int j) {
+    public static void swap(int[] arr, int i, int j) {
         int temp = arr[i];
         arr[i] = arr[j];
         arr[j] = temp;
