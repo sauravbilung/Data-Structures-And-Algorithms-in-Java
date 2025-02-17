5e6590d5d90b022cf4375969ac9250469a7820ed
sauravbilung <saurav.bilung@gmail.com>
2025-02-17 16:16:17 +0530

    Added code for interval insertion

---
+++ src/main/java/patterns/overlappingIntervals/intervalInsertion/_1_InsertInterval.java
@@ -0,0 +1,61 @@
+package patterns.overlappingIntervals.intervalInsertion;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class _1_InsertInterval {
+
+    public static void main(String[] args) {
+        InsertIntervalSolution solution = new InsertIntervalSolution();
+        int[][] intervals1 = {{1, 2}, {3, 5}, {6, 7}, {8, 10}, {12, 16}};
+        int[] newInterval1 = {4, 8};
+        int[][] intervals2 = {{1, 2}, {3, 5}, {6, 7}, {8, 10}, {12, 16}};
+        int[] newInterval2 = {4, 8};
+        int[][] intervals3 = {{1, 2}, {4, 5}, {6, 7}, {8, 10}, {12, 16}};
+        int[] newInterval3 = {3, 8};
+        int[][] intervals4 = {{1, 2}, {4, 7}, {8, 10}, {12, 16}};
+        int[] newInterval4 = {3, 5};
+        int[][] intervals5 = {{1, 2}, {5, 7}, {8, 10}, {12, 16}};
+        int[] newInterval5 = {3, 5};
+        int[][] intervals6 = {{1, 5}};
+        int[] newInterval6 = {2, 3};
+        int[][] intervals7 = {{2, 3}};
+        int[] newInterval7 = {1, 5};
+        System.out.println("Intervals :");
+        Arrays.stream(intervals4).forEach(row -> System.out.printf("%s ", Arrays.toString(row)));
+        System.out.printf("\nNew interval : %s", Arrays.toString(newInterval4));
+        System.out.println("\nResult :");
+        Arrays.stream(solution.insert(intervals4, newInterval4)).forEach(row -> System.out.printf("%s ", Arrays.toString(row)));
+    }
+}
+
+class InsertIntervalSolution {
+    public int[][] insert(int[][] intervals, int[] newInterval) {
+        List<int[]> result = new ArrayList<>();
+        int i = 0;
+
+        // Add all intervals before new interval
+        while (i < intervals.length && intervals[i][1] < newInterval[0]) {
+            result.add(intervals[i]);
+            i++;
+        }
+
+        // Merge overlapping intervals
+        while (i < intervals.length && intervals[i][0] <= newInterval[1]) {
+            newInterval[0] = Math.min(newInterval[0], intervals[i][0]);
+            newInterval[1] = Math.max(newInterval[1], intervals[i][1]);
+            i++;
+        }
+
+        result.add(newInterval);
+
+        // Add remaining intervals to result
+        while (i < intervals.length) {
+            result.add(intervals[i]);
+            i++;
+        }
+
+        return result.toArray(new int[result.size()][]);
+    }
+}
